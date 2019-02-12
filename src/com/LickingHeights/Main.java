25c6da43c2f51f5fb1e106ecdf051d2798dea283
Sandra <sandra.adamah-tassah.2021@studen.lhschools.org>
2019-02-12 14:08:09 -0500

    Created scanner

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,14 @@
+package com.LickingHeights;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner keyboard;
+        //phase 2 (initalizing)
+        keyboard = new Scanner(System.in);
+
+    }
+}
