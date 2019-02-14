bf76406680f81f4cf3173f55b3216d221a48e9a9
Sandra <sandra.adamah-tassah.2021@studen.lhschools.org>
2019-02-14 13:13:49 -0500

    Created and initialized variables for height

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -10,5 +10,19 @@ public class Main {
         //phase 2 (initalizing)
         keyboard = new Scanner(System.in);
 
+        int heightInFeet, heightInInches;
+
+        System.out.println("What is your height? (in feet)");
+        heightInFeet = keyboard.nextInt();
+
+        System.out.println("What is your height? (in inches)");
+        heightInInches = keyboard.nextInt();
+
+
+
+
+        //bmi = weight(in kilograms) / height(in centimeters) squared
+        // 1 in = 0.0254 meter
+
     }
 }
